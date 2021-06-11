2ce05a6e742ecc5754c207be87d3075e64c3394e
Alex Covizzi <alexcovizzi@gmail.com>
2021-06-11 18:08:45 +0200

    Change version to 0.3.0

--- app/__meta__.py
+++ app/__meta__.py
@@ -1,5 +1,5 @@
 __app__ = 'WorkBreakTimer'
 __display__ = 'Work/Break Timer'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 __repo__ = 'https://github.com/AlexCovizzi/WorkBreakTimer'
 __issues__ = 'https://github.com/AlexCovizzi/WorkBreakTimer/issues'
